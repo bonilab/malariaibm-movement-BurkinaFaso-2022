09aa6d5eab8fc172db7ca3cb106c96c12cd04caf
rjzupkoii <rjzii@yahoo.com>
2022-12-19 11:57:25 -0500

    Finalize script for Fig. S5

--- Source/Analysis/get_stderr.py
+++ Source/Analysis/get_stderr.py
@@ -21,7 +21,6 @@ from utility import progressBar
 
 # General constants for the script
 CONNECTION = "host=masimdb.vmhost.psu.edu dbname=burkinafaso user=sim password=sim"
-LOCATIONS = '../../Data/Movement/analysis_cells.csv'
 REPLICATE_COUNT = 50
 
 # Database study ids
@@ -29,11 +28,9 @@ MODEL = 17551
 MARSHALL = 17546
 
 # Data filenames
-MERGED_CSV = '../../Data/Movement/merged_data.csv'
-MODEL_CSV = '../../Data/Movement/model_data.csv'
+LOCATIONS = '../../Data/Movement/analysis_cells.csv'
 MARSHALL_CSV = '../../Data/Movement/marshall_data.csv'
-RAW_MODEL_CSV = '../../Data/Movement/raw_model_data.csv'
-RAW_MARSHALL_CSV = '../../Data/Movement/raw_marshall_data.csv'
+MODEL_CSV = '../../Data/Movement/model_data.csv'
 
 
 def get_replicate_data(replicateId, filter):
@@ -89,43 +86,13 @@ def process(cells, population, model, filename):
             ))
 
 
-def plot_scatter_trips():
-    # Load the data from disk
-    data = pd.read_csv(MERGED_CSV, sep=',', header=0)
-    data = data.sort_values(by='model_mean', ascending=False)
-    print(data)
-
-    # Prepare the plot
-    matplotlib.rc_file('matplotlib-scatter')
-    fig, plot = plt.subplots()
-        
-    x = range(0, len(data['population']))
-    plot.errorbar(x, data['marshall_mean'], data['marshall_sd'], 
-                  linestyle='None', ecolor='black', elinewidth=1.5, zorder=1)    
-    plot.scatter(x, data['marshall_mean'], c=data['population'],
-                 label='Marshall et al. Model',
-                 cmap='gist_gray', edgecolors='black', zorder=3)        
-    
-    plot.errorbar(x, data['model_mean'], data['model_sd'], 
-                  linestyle='None', ecolor='black', elinewidth=1.5, zorder=2)
-    sp = plot.scatter(x, data['model_mean'], c=data['population'], 
-                      label='Mathmatical Model with Travel Surface',
-                      marker='s', cmap='gist_gray', edgecolors='black', zorder=4)    
-    
-    plot.set_xlim([-1, len(data['model_mean']) + 1])
-    plot.set_ylim([0, max(data['model_mean']) + 100])
-    plot.set_ylabel('Mean Trips to Cell')
-    plot.xaxis.set_major_formatter(plt.NullFormatter())
-    plot.legend(frameon=False)
-    
-    cb = fig.colorbar(sp)
-    cb.set_label('Cell Population')
-
-    fig.tight_layout()
-    fig.savefig('out/testing.png')
-
-
 def plot_scatter():
+    def format_ticks(ticks):
+        labels = []
+        for tick in [math.pow(10, value) for value in ticks]:
+            labels.append(int(tick))
+        return labels  
+
     # Load the data from disk
     model = pd.read_csv(MODEL_CSV, sep=',', header=0)
     marshall = pd.read_csv(MARSHALL_CSV, sep=',', header=0)
@@ -138,25 +105,13 @@ def plot_scatter():
     x = np.log10(model['population'])
     plot.errorbar(x, marshall['mean'], marshall['std'], 
                     linestyle='None', ecolor='black', elinewidth=1.5, zorder=1)    
-    plot.scatter(x, marshall['mean'], label='Marshall et al. Model',
+    plot.scatter(x, marshall['mean'], label='Marshall et al. Model (see Fig. 3a)',
                     facecolors='#5A5A5A', edgecolors='black', zorder=3)        
     plot.errorbar(x, model['mean'], model['std'], 
                     linestyle='None', ecolor='black', elinewidth=1.5, zorder=2)
-    plot.scatter(x, model['mean'], label='Mathmatical Model with Travel Surface',
+    plot.scatter(x, model['mean'], label='Mathematical Model With Best Fit and Travel Surface (see Fig. 3b)',
                     facecolors='#D3D3D3', edgecolors='black', zorder=4)    
-    
-    # # Plot by mean and population    
-    # plot.errorbar(marshall['mean'], marshall['population'], marshall['std'], 
-    #                 linestyle='None', ecolor='black', elinewidth=1.5, zorder=1)    
-    # plot.scatter(marshall['mean'], marshall['population'], label='Marshall',
-    #                 facecolors='#5A5A5A', edgecolors='black', zorder=3)        
-    # plot.errorbar(model['mean'], model['population'], model['std'], 
-    #                 linestyle='None', ecolor='black', elinewidth=1.5, zorder=2)
-    # plot.scatter(model['mean'], model['population'], label='PSU',
-    #                 facecolors='#D3D3D3', edgecolors='black', zorder=4)
-    # plot.set_ylim([0, 220000])
-    # plot.set_xlim([0, 4100])
-    
+        
     
     # Add the labeled points to the map
     plot.annotate('Bobo-Dioulasso', (np.log10(56000), 3400))        # Bobo-Dioulasso, population = 57734, trips = 3474.14
@@ -175,45 +130,8 @@ def plot_scatter():
     plot.legend(frameon=False)
     
     fig.tight_layout()
-    fig.savefig('out/Fig. 5 - 300 dpi.png')
-
-def format_ticks(ticks):
-    labels = []
-    for tick in [math.pow(10, value) for value in ticks]:
-        labels.append(int(tick))
-    return labels  
-
-
-# Generate the plot from the data sets
-def plot_boxplot():
-    # Load the data from disk
-    model = pd.read_csv(RAW_MODEL_CSV, sep=',', header=None)
-    x  = model.loc[:,1].tolist()
-    model = model.drop(columns=[0, 1])
-    marshall = pd.read_csv(RAW_MARSHALL_CSV, sep=',', header=None)
-    marshall = marshall.drop(columns=[0, 1])
-
-    # Prepare the plot
-    matplotlib.rc_file('matplotlib-scatter')
-    fig, plot = plt.subplots()
-    
-    # Add the data to the plot
-    bp_model = plot.boxplot(model.T, 
-                            showfliers=False, notch=True, patch_artist=True, 
-                            boxprops=dict(facecolor='#D3D3D3', color='black'), medianprops=dict(color='black'))
-    bp_marshall = plot.boxplot(marshall.T, 
-                               showfliers=False, notch=True, patch_artist=True, 
-                               boxprops=dict(facecolor='#5A5A5A', color='black'), medianprops=dict(color='black'))
-
-    # Format the plot
-    plot.set_ylabel('Trips to Cell')
-    plot.set_ylim([0, 4500])
-    plot.tick_params(axis='x', which='both', bottom=False, top=False, labelbottom=False)
-    plot.legend([bp_model["boxes"][0], bp_marshall["boxes"][0]], ['PSU', 'Marshall'], loc='upper right', frameon=False)
-
-    fig.tight_layout()
-    fig.savefig('working.png')
-    
+    fig.savefig('out/ESM Fig. S5 - 300 dpi.png', dpi=300)
+   
 
 def main(load):
     # Read the population and cells that were selected
@@ -233,9 +151,9 @@ def main(load):
         print('\nData load complete!')
 
     # Generate the plots with the saved data
-    plot_scatter_trips()
+    plot_scatter()
 
 
 if __name__ == '__main__':
     if not os.path.exists('out'): os.mkdir('out')
-    main(False)
+    main(True)
